a29b5b2581fb982197c31c5abb87b428581f81e8
Taleb Hammoudé <taleb.hammoude@outlook.com>
2021-12-03 17:45:04 +0100

    f

---
+++ app.js
@@ -0,0 +1,41 @@
+const KEY = '313698e5d41cebecef7dd7a6fb93043d';
+let searchText = '';
+
+
+const searchField = document.querySelector("#searchpic");
+const searchButton = document.querySelector("#searchBtn");
+
+
+searchField.addEventListener("input", (e)=> {
+   searchText =  e.target.value;
+   search();
+})
+
+
+
+function search () {
+    searchButton.addEventListener("click", ()=> {
+        
+        console.log(searchText);
+
+
+
+    })
+}
+
+
+const url = `https://www.flickr.com/services/rest/?api_key=${KEY}&method=flickr.photos.search&text=${searchText}&format=json&nojsoncallback=1&per_page=1&page=1`;
+
+
+async function getData () {
+    
+    const response = await fetch(url);
+    const data = await response.json();
+
+    return data;
+}
+
+
+getData().then((data)=>{
+    console.log(data);
+});
